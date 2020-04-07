90a6df7246ba3a440b3b66e91a60e30f570dfd75
Mia5391 <mariakrasteva5391@gmail.com>
2020-04-06 22:40:50 -0400

    using axios to set default base url, and headers (Auth and post content type)

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -15,7 +15,7 @@ class NewPost extends Component {
             body: this.state.content, 
             author: this.state.author
         };
-        axios.post("https://jsonplaceholder.typicode.com/posts", newPost)
+        axios.post("/posts", newPost)
         .then(response => {
             console.log(response);
         });
