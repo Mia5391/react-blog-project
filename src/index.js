552b71d17b424b24a457f0d27f456e85b04b110f
Mia5391 <mariakrasteva5391@gmail.com>
2020-04-06 23:20:03 -0400

    axios instance split configuration

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import registerServiceWorker from './registerServiceWorker';
 
 axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 axios.defaults.headers.common['Authorizastion'] = 'AUTH TOKEN';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Content-Type'] = 'application/json'; 
 
 axios.interceptors.request.use(request => {
     console.log(request);
